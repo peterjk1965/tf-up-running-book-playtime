18df87a8eb963c297a03fb517ace01b64d11a383
peterjk1965 <peter.j.k@hotmail.com>
2024-03-08 11:35:43 -0600

    changes to main

--- main.tf
+++ main.tf
@@ -1,16 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-2"  
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-022661f8a4a1b91cf"
+  ami                    = "ami-0f5daaa3a7fb3378b"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello World!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
   tags = {
     Name = "terraform-example"
@@ -21,15 +29,20 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["69.136.168.38/32"]
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+}
+
+variable "server_port" {
+  description = "Port used for http requests"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value        = aws_instance.example.public_ip
+  description  = "Public ip address"
 }
